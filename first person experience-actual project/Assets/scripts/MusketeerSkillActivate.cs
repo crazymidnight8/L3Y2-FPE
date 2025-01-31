c8a152cef44a8bc780e1e79d38b12f49a21d50e8
crazymidnight5 <167751030+crazymidnight5@users.noreply.github.com>
2025-01-31 15:56:33 +0000

    paths update

--- first person experience-actual project/Assets/scripts/MusketeerSkillActivate.cs
+++ first person experience-actual project/Assets/scripts/MusketeerSkillActivate.cs
@@ -47,23 +47,23 @@ public class MusketeerSkillActivate : MonoBehaviour
         }
     }
 
-    public GameObject FindClosestEnemy()
-    {
-        GameObject[] gos;
-        gos = GameObject.FindGameObjectWithTag("enemy");
-        GameObject closest = null;
-        float distance = Mathf.Infinity;
-        Vector3 position = transform.position;
-        foreach (GameObject go in gos)
-        {
-            Vector3 diff = go.transform.position - position;
-            float curDistance = diff.sqrMagnitude;
-            if (curDistance < distance)
-            {
-                closest = go;
-                distance = curDistance;
-            }
-        }
-        return closest;
-    }
+    // public GameObject FindClosestEnemy()
+    // {
+    //     GameObject[] gos;
+    //     gos = GameObject.FindGameObjectWithTag("enemy");
+    //     GameObject closest = null;
+    //     float distance = Mathf.Infinity;
+    //     Vector3 position = transform.position;
+    //     foreach (GameObject go in gos)
+    //     {
+    //         Vector3 diff = go.transform.position - position;
+    //         float curDistance = diff.sqrMagnitude;
+    //         if (curDistance < distance)
+    //         {
+    //             closest = go;
+    //             distance = curDistance;
+    //         }
+    //     }
+    //     return closest;
+    // }
 }
